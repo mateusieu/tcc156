6db18d608a17163cb919ce72b0e2b4e69487ab35
Mateus Gabriel <mateusieu1@gmail.com>
2020-10-31 15:23:33 -0300

    pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number[] = [350,320,150];
+  pieChartLabels: string[] = ['Menó', 'Pai', 'Rei' ];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c','#ff6b6b','#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+  pieChartType = 'doughnut';
+
   ngOnInit(): void {
   }
 
